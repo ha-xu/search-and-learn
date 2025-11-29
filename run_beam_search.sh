c3964e86420970db0e17f68d1bd6c42542d8b700
xzh <xzh@applent.com>
2025-11-29 14:24:27 -0500

    feat: extend job time limit in beam search script to 8 hours

--- run_beam_search.sh
+++ run_beam_search.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=csci_ga_3033_szhang-2025fa
 #SBATCH --partition=c12m85-a100-1
 #SBATCH --gres=gpu:1
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --mem=64G  
 #SBATCH --job-name=beam_search
 #SBATCH --output=/scratch/zx1875/slurm_logs/%x-%j.out
