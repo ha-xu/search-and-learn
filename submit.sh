7b2332c2b24f21f065cc8daf8e9a86df9e924c73
xzh <xzh@applent.com>
2025-12-06 21:18:29 -0500

    feat: increase memory allocation to 128G in submit script

--- submit.sh
+++ submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=c12m85-a100-1
 #SBATCH --gres=gpu:1
 #SBATCH --time=04:00:00
-#SBATCH --mem=64G  
+#SBATCH --mem=128G  
 #SBATCH --job-name=search_and_learn
 #SBATCH --output=/scratch/zx1875/slurm_logs/%x-%j.out
 #SBATCH --error=/scratch/zx1875/slurm_logs/%x-%j.err
