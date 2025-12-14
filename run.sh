64491513457ed5b202e274867c00064bc0c139d1
xzh <xzh@applent.com>
2025-12-14 15:29:00 -0500

    fix: uncomment script execution lines in run.sh for clarity and execution flow

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-bash run_beam_search.sh
-bash run_beam_search_dynamic.sh
-# bash run_dvts_dynamic.sh
-bash run_beam_search_dynamic_plus.sh
+# bash run_beam_search.sh
+# bash run_beam_search_dynamic.sh
+# # bash run_dvts_dynamic.sh
+# bash run_beam_search_dynamic_plus.sh
 # bash run_dvts.sh
 
 # bash run_print_scores.sh
+
+bash run_beam_search_dynamic_official.sh
